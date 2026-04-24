466af121cdb6eb02edfb9ad8fb5fed56d6ab29fd
Rakib Hosain <rakibhosain2302@gmail.com>
2026-04-24 23:41:06 +0600

    SPA Wanted Section Content Add

--- src/components/WantSection.jsx
+++ src/components/WantSection.jsx
@@ -1,14 +1,30 @@
 
 const WantSection = () => {
-  return (
-    <>
-        <div className="container wanted-section">
-            <h1 className="text-center mb-5">What We Want</h1>
-            <p>As industry stalwarts with a decade of experience, we seek determined young professionals eager to grow in these areas:</p>
-        </div>
-      
-    </>
-  )
+    return (
+        <>
+            <div className="container wanted-section">
+                <h1 className="text-center mb-5">What We Want</h1>
+                <p>As industry stalwarts with a decade of experience, we seek determined young professionals eager to grow in these areas:</p>
+                <div className="card-custom">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <h2 className="title">IT</h2>
+                            <p>Enabled Services</p>
+                        </div>
+                        <div className="col-md-4">
+                            <h2 className="title">Client</h2>
+                            <p>Management</p>
+                        </div>
+                        <div className="col-md-4">
+                            <h2 className="title" >Sales</h2>
+                            <p>Solutions</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
 }
 
 export default WantSection
